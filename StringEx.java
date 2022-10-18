1fbabbee4edaa692be1f304b230593a1e9570c4a
krishnasai33 <saikrishna33@sai.gmail.com>
2022-10-18 15:21:30 +0530

    New program StringEx is added

---
+++ StringEx.java
@@ -0,0 +1,13 @@
+public class StringEx{
+	public static void main (String args[]){
+		String str = new String("surya sai krishna");
+		System.out.println(str.length());
+		System.out.println(str.toUpperCase());
+		
+		System.out.println(str.indexOf("i"));
+		System.out.println(str.charAt(4));
+		
+
+}
+
+}
